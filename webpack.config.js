18236eb9f0eae98ee317ce52664a01ee69e1e728
Sergio Yarkov <channel.my.honey@gmail.com>
2024-11-05 19:19:32 +0300

    Первый Коммит

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { DefinePlugin } = require('webpack')
